10412b8d3d013d3ac8e77cd1ea346912045789ad
Tormod Flesjø <tormod.flesjo@gmail.com>
2025-05-12 18:07:37 +0200

    add eir

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/17mai2025/', // Replace 'your-repository' with the name of your repo
+});
