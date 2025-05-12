b940194591422e11cfccf79411715f1e4feb6a87
Tormod Flesjø <tormod.flesjo@gmail.com>
2025-05-12 18:11:56 +0200

    add homepage

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/17mai2025/', // Replace 'your-repository' with the name of your repo
+  base: '/17mai2025', // Replace 'your-repository' with the name of your repo
 });
