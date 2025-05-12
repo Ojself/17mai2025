6305e85574ffffecadb4ce120706ed1802c54e9c
Tormod Flesjø <tormod.flesjo@gmail.com>
2025-05-12 18:03:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+
+import guleHusetImage from './assets/gulehuset_landscape.png';
+import hostImage from './assets/frimodig.png';
+
+function App() {
+  return (
+    <div className="appContainer">
+      {/* Hero Section */}
+      <div className="imageContainer">
+        <img className="imgHero" src={guleHusetImage} alt="Det Gule Huset" />;
+      </div>
+      <div className="imageHostContainer">
+        <img className="imgHost" src={hostImage} alt="Vertskapet" />;
+      </div>
+
+      {/* Title and Description */}
+      <div className="titleContainer">
+        <h1 className="title">17. mai-fest i Det Gule Huset</h1>
+        <a
+          href="https://maps.app.goo.gl/3sZv5LDSwMVTq5jXA"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="address"
+        >
+          Dops Gate 12
+        </a>
+      </div>
+      <div className="detailsAndMapContainer">
+        <div className="detailsContainer">
+          <p className="">
+            Kom og bli med på feiringen av 17. mai i Det Gule Huset! Vi åpner
+            dørene kl. 14:00 og holder åpent til sent. Så hvis du er i området
+            og ønsker å feire nasjonaldagen med oss, er du hjertelig velkommen!
+          </p>
+        </div>
+        <div className="mapContainer">
+          <iframe
+            className="map"
+            src="https://www.openstreetmap.org/export/embed.html?bbox=10.746364295482637%2C59.919230131826765%2C10.74881047010422%2C59.920308305237434&amp;layer=transportmap&amp;marker=59.91976922291057%2C10.747587382793427"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
