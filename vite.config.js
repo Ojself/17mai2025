0f85ae18ed6fa317621fdf9841ff2866872b3166
Tormod Flesjø <tormod.flesjo@gmail.com>
2025-05-12 18:16:45 +0200

    remove stuff

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/17mai2025', // Replace 'your-repository' with the name of your repo
 });
